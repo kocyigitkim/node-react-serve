4b6d19fcead73fd89d951d411ebc774b896c5e80
Muhammed Koçyiğit <muhammetkocyigit0@gmail.com>
2021-08-11 13:48:34 +0300

    asset load bug fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { default: fetch } = require("node-fetch");
 const path = require("path");
 const fs = require("fs");
 const express = require("express");
+const ws = require('ws');
 const rMatchFilePath = /([^\":]+)\.(js|s?css|ico|icon|bmp|jpg|png|zip|gif|svg|pdf|docx|doc|xls|xlsx|xlsm|ppt|pptx|txt|json|xml|chunk|ttf|woff|woff2|map)/g;
 const rMatchFile = /(href|src)=\"([^\":]+)\.(js|s?css|ico|icon|bmp|jpg|zip|png|svg|gif|pdf|docx|doc|xls|xlsx|xlsm|ppt|pptx|txt|json|xml|chunk|ttf|woff|woff2|map)\"/g;
 
@@ -36,7 +37,7 @@ function InitProductionServer(app, basePath, buildPath, port) {
       basePath = longPath[0];
       if (!baseUrl.toLowerCase().startsWith(longPath[0].toLowerCase())) _newpath = longPath[0] + baseUrl;
     }
-    
+
     const filePath = path.join(buildPath, baseUrl);
     const extension = path.extname(baseUrl);
     if (fs.existsSync(filePath) && extension && extension.length >= 1) {
@@ -53,16 +54,66 @@ function InitProductionServer(app, basePath, buildPath, port) {
   });
 }
 
+function InitReactWebSocket(app, basePath, port) {
+  const b = new ws(`ws://localhost:${port}/sockjs-node`, {});
+  const wss = new ws.Server({ server: app, path: '/sockjs-node' });
+
+
+  app.use(async (req, res, next) => {
+
+    if (req.url === "/sockjs-node") {
+
+      try {
+        wss.handleUpgrade(req, req.socket, req.headers, async (ws) => {
+          b.emit(ws);
+          ws.on('message', (message) => {
+            b.send(message);
+          });
+          b.on('message', (message) => {
+            var msg = message.toJSON();
+            var result = {};
+            var decoded = Buffer.from(msg, { encoding: 'utf-8' });
+
+            ws.send(decoded.toString("utf-8"));
+          });
+        });
+
+
+        return;
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    next();
+  });
+}
+
 function InitDevelopmentServer(app, basePath, port) {
-  app.use(basePath + "*", async (req, res, next) => {
+
+  try {
+    InitReactWebSocket(app, basePath, port);
+  } catch (e) {
+    console.error(e);
+  }
+
+  app.use("*", async (req, res, next) => {
     var _newpath = req.baseUrl.toString().replace(/\/\//g, "/");
     const referer = req.headers.referer;
+    var longPath = [];
     if (referer && referer.length > 0) {
       var pathname = new URL(referer).pathname;
-      var longPath = registeredBasePaths.filter(bp => pathname.toLowerCase().startsWith(bp[0].toLowerCase())).sort((a, b) => a[0].length - b[0].length).reverse()[0];
-      basePath = longPath[0];
-      port = longPath[1];
-      if (!_newpath.toLowerCase().startsWith(longPath[0].toLowerCase())) _newpath = longPath[0] + _newpath;
+      try {
+        longPath = registeredBasePaths.filter(bp => pathname.toLowerCase().startsWith(bp[0].toLowerCase())).sort((a, b) => a[0].length - b[0].length).reverse()[0];
+        basePath = longPath[0];
+        port = longPath[1];
+        if (!_newpath.toLowerCase().startsWith(longPath[0].toLowerCase())) _newpath = longPath[0] + _newpath;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    if ((!referer || longPath.length === 0) && !_newpath.toLowerCase().startsWith(basePath.toLowerCase())) {
+      next();
+      return;
     }
     basePath = basePath.replace(/\/\//g, "/");
     if (basePath.startsWith("/")) {
@@ -74,16 +125,21 @@ function InitDevelopmentServer(app, basePath, port) {
     if (_newpath.toLowerCase().startsWith(basePath.toLowerCase())) {
       _newpath = _newpath.substr(basePath.length);
     }
+    else {
+      _newpath = "/" + _newpath;
+    }
     var isIndex = false;
     if (_newpath.trim().length === 0) {
       isIndex = true;
     }
-    _newpath = "/" + _newpath;
 
+    if (_newpath.indexOf(".") === -1) {
+      isIndex = true;
+    }
 
-    var _res = await fetch("http://localhost:" + port + _newpath, {
+    var _res = await fetch("http://localhost:" + port.toString() + _newpath, {
       method: req.method,
-      headers: req.headers,
+      headers: { ...req.headers, referer: null, host: null },
     }).catch(console.error);
     if (_res) {
       for (var h of _res.headers.keys()) {
